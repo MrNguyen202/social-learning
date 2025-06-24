8a6f12116188b6c39ac806cc1bd64ca3a6593055
Bao44 <tqbao44@gmail.com>
2025-06-24 13:07:17 +0700

    style: install framer-motion

--- web/app/chatbot/ChatBotAI.tsx
+++ web/app/chatbot/ChatBotAI.tsx
@@ -6,30 +6,23 @@ function ChatBotAI() {
   const [showChatIcon, setShowChatIcon] = useState(true);
   const chatIconRef = useRef<HTMLButtonElement>(null);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 200) {
-        setShowChatIcon(true);
-      } else {
-        setShowChatIcon(false);
-      }
-    };
-
-    handleScroll();
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   const toggleChat = () => {
     setIsChatOpen(!isChatOpen);
   };
   return (
     <div className="flex flex-col min-h-screen">
-
+      <div
+        className="fixed bottom-4 right-4 z-50
+      "
+      >
+        <button
+          ref={chatIconRef}
+          onClick={toggleChat}
+          className="rounded-full size-14 p-2 shadow-lg bg-black text-white"
+        >
+          {!isChatOpen ? <p>On</p> : <p>Off</p>}
+        </button>
+      </div>
     </div>
   );
 }
