72a802726fbb35e4e1b1cdb00e7cac97ff3f9f2e
Bao44 <tqbao44@gmail.com>
2025-09-21 12:22:24 +0700

    setup: add component

---
+++ app/src/components/PostCard.tsx
@@ -0,0 +1,305 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Alert,
+  Share,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import Avatar from './Avatar';
+import moment from 'moment';
+import RenderHTML from 'react-native-render-html';
+import Video from 'react-native-video';
+// import Loading from './Loading';
+// import { createPostLike, removePostLike } from '../api/post';
+// import { downloadFileShare, getSupabaseFileUrl } from '../api/image';
+import { theme } from '../../constants/theme';
+import { hp, wp } from '../../helpers/common';
+import {
+  Delete,
+  Edit,
+  Forward,
+  Heart,
+  MessageCircle,
+  MoreHorizontal,
+} from 'lucide-react-native';
+
+const textStyle = {
+  color: theme.colors.dark,
+  fontSize: hp(1.75),
+};
+const tagsStyles = {
+  div: textStyle,
+  p: textStyle,
+  ol: textStyle,
+  h1: {
+    color: theme.colors.dark,
+  },
+  h4: {
+    color: theme.colors.dark,
+  },
+};
+const PostCard = ({
+  item,
+  currentUser,
+  router,
+  hasShadow = true,
+  showMoreIcon = true,
+  showDelete = false,
+  onDelete = () => {},
+  onEdit = () => {},
+}: any) => {
+  const style = {
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.06,
+    shadowRadius: 6,
+    elevation: 1,
+  };
+
+  const [likes, setLikes] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLikes(item?.postLikes);
+  }, []);
+
+  const openPostDetails = () => {
+    if (!showMoreIcon) return null;
+    router.push({ pathname: 'postDetails', params: { postId: item?.id } });
+  };
+
+  // const onLike = async () => {
+  //   if (liked) {
+  //     //remove
+  //     let updatedLikes = likes.filter((like) => like.userId != currentUser?.id);
+  //     setLikes([...updatedLikes]);
+  //     let res = await removePostLike(item?.id, currentUser?.id);
+  //     console.log("removed like", res);
+  //     if (!res.success) {
+  //       Alert.alert("Post", "Something went wrong!");
+  //     }
+  //   } else {
+  //     // create
+  //     let data = {
+  //       userId: currentUser?.id,
+  //       postId: item?.id,
+  //     };
+  //     setLikes([...likes, data]);
+  //     let res = await createPostLike(data);
+  //     console.log("added like", res);
+  //     if (!res.success) {
+  //       Alert.alert("Post", "Something went wrong!");
+  //     }
+  //   }
+  // };
+
+  // const onShare = async () => {
+  //   let content = { message: stripHtmlTags(item?.body) };
+  //   if (item?.file) {
+  //     // downdload file
+  //     setLoading(true);
+  //     let url = await downloadFileShare(getSupabaseFileUrl(item?.file).uri);
+  //     setLoading(false);
+  //     content.url = url;
+  //   }
+  //   Share.share(content);
+  // };
+
+  const handlePostDelete = () => {
+    Alert.alert('Confirm', 'Are you sure you want to do this?', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('modal cancelled'),
+        style: 'cancel',
+      },
+      {
+        text: 'Delete',
+        onPress: () => onDelete(item),
+        style: 'destructive',
+      },
+    ]);
+  };
+
+  const createAt = moment(item?.created_at).format('MMM D');
+  // const liked = likes.filter((like) => like.userId == currentUser?.id)[0]
+  //   ? true
+  //   : false;
+
+  return (
+    <View style={[styles.container, hasShadow && style]}>
+      <View style={styles.header}>
+        {/* user info and post time */}
+        <View style={styles.userInfo}>
+          <Avatar
+            size={hp(4.5)}
+            uri={item?.user?.avatar}
+            rounded={theme.radius.md}
+          />
+          <View style={{ gap: 2 }}>
+            <Text style={styles.username}>{item?.user?.name}</Text>
+            <Text style={styles.postTime}>{createAt}</Text>
+          </View>
+        </View>
+
+        {showMoreIcon && (
+          <TouchableOpacity onPress={openPostDetails}>
+            <MoreHorizontal
+              size={hp(3)}
+              strokeWidth={3}
+              color={theme.colors.text}
+            />
+          </TouchableOpacity>
+        )}
+
+        {showDelete && currentUser.id == item?.userId && (
+          <View style={styles.actions}>
+            <TouchableOpacity onPress={() => onEdit(item)}>
+              <Edit size={hp(2.5)} color={theme.colors.text} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handlePostDelete}>
+              <Delete size={hp(2.5)} color={theme.colors.rose} />
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+
+      {/* post body & media */}
+
+      <View style={styles.content}>
+        {/* <View style={styles.postBody}>
+          {item?.body && (
+            <RenderHTML
+              contentWidth={wp(100)}
+              source={{ html: item?.body }}
+              tagsStyles={tagsStyles}
+            />
+          )}
+        </View> */}
+
+        {/* post image */}
+        {/* {item?.file && item?.file?.includes('postImages') && (
+          <Image
+            source={getSupabaseFileUrl(item?.file)}
+            transition={100}
+            style={styles.postMedia}
+            contentFit="cover"
+          />
+        )} */}
+
+        {/* post video */}
+        {/* {item?.file && item?.file?.includes('postVideos') && (
+          <Video
+            source={getSupabaseFileUrl(item?.file)}
+            style={[styles.postMedia, { height: hp(30) }]}
+            controls={true} // Hiển thị các điều khiển phát
+            resizeMode="cover"
+            repeat={true} // Lặp lại video
+          />
+        )} */}
+      </View>
+
+      {/* like, comment & share */}
+      {/* <View style={styles.footer}>
+        <View style={styles.footerButton}>
+          <TouchableOpacity onPress={onLike}>
+            <Heart
+              size={24}
+              fill={liked ? theme.colors.rose : theme.colors.textLight}
+            />
+          </TouchableOpacity>
+          <Text style={styles.count}>{likes?.length}</Text>
+        </View>
+        <View style={styles.footerButton}>
+          <TouchableOpacity onPress={openPostDetails}>
+            <MessageCircle size={24} color={theme.colors.textLight} />
+          </TouchableOpacity>
+          <Text style={styles.count}>{item?.comments[0]?.count}</Text>
+        </View>
+        <View style={styles.footerButton}>
+          {loading ? (
+            <Loading size="small" />
+          ) : (
+            <TouchableOpacity onPress={onShare}>
+              <Forward size={24} color={theme.colors.textLight} />
+            </TouchableOpacity>
+          )}
+        </View>
+      </View> */}
+    </View>
+  );
+};
+
+export default PostCard;
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 10,
+    marginBottom: 15,
+    borderRadius: theme.radius.xl,
+    borderCurve: 'continuous',
+    padding: 10,
+    paddingVertical: 12,
+    backgroundColor: 'white',
+    borderWidth: 0.5,
+    borderColor: theme.colors.gray,
+    shadowColor: '#000',
+    marginHorizontal: wp(2),
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  username: {
+    fontSize: hp(1.7),
+    color: theme.colors.textDark,
+    fontWeight: theme.fonts.medium,
+  },
+  postTime: {
+    fontSize: hp(1.4),
+    color: theme.colors.textLight,
+    fontWeight: theme.fonts.medium,
+  },
+  content: {
+    gap: 10,
+    // marginBottom: 10,
+  },
+  postMedia: {
+    height: hp(40),
+    width: '100%',
+    borderRadius: theme.radius.xl,
+    borderCurve: 'continuous',
+  },
+  postBody: {
+    marginLeft: 5,
+  },
+  footer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
+  },
+  footerButton: {
+    marginLeft: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+  },
+  actions: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 18,
+  },
+  count: {
+    color: theme.colors.text,
+    fontSize: hp(1.8),
+  },
+});
